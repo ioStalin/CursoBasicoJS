f0f05fca191cc211f85eb397a87ad7f26360332a
Stalin Yaguana <stalinyaguana@hotmail.com>
2023-04-01 14:02:11 -0500

    Se anadieron archivos para reisoar formatos de declaracaión de funciones

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Oscar";
+
+//Declarar
+var edad;
+
+//Inicializar
+edad=30;
+
+edad
+
+var elementos = ["Compu","movil"];
+
+var objeto_persona = {
+    nombre: "Diego",
+    edad: 30
+
+}
+
+objeto_persona
