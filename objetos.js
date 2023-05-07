91853fa8931e0a07953ded160756eff540721221
Stalin Yaguana <stalinyaguana@hotmail.com>
2023-05-07 12:11:26 -0500

    Se realiz[o varios eejmplos para practicar la creaci[on de objetos, todo objeto tiene Datos y Funcionalidad o lo que es lo mismo Atributos y metodos

---
+++ objetos.js
@@ -0,0 +1,22 @@
+/*
+var miAuto = {
+    marca : "FIAT",
+    modelo : "Pulse",
+    anio : 2023,
+    color : "Rojo",
+    detallesDelAuto: function(){
+        console.log(`Auto ${this.modelo} ${this.anio} ${this.color}`);
+    }
+};             /*formato para crear objeto
+*/
+
+var miCasa = {
+    tipo : "Concreto",
+    ubicacion : "Quito",
+    numDepartamentos : 6,
+    garage : "no",
+    detallesCasa: function(){
+        console.log(`A continuacion detalles de casa: Tipo ${this.tipo} ${this.ubicacion} ${this.numDepartamentos}`);
+
+    }
+};
